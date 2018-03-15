15657e5cfb6a9f6610a56fd6b78028903e7248cc
Fleur Robichez <fleur.robichez@student.arteveldehs.be>
2018-03-15 14:02:33 +0100

    week 5

---
+++ week 5/oefening 2/script/custom.js
@@ -0,0 +1,15 @@
+var books = [
+    {title: 'Harry Potter',
+    author: 'J.K. Rowling',
+    alreadyRead: false
+    },
+    {title: 'Jane Eyre',
+    author: 'Charlotte Brontë',
+    alreadyRead: true
+    },
+    {title: 'De verschrikkelijke schoolmeester.',
+    author: 'Dolf Verroen',
+    alreadyRead: true
+    }
+];
+
